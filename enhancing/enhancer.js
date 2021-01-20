49f7f7ffe0541f7a92993c0983b7fc578f7f1cdc
dwainejade <dwainejade@gmail.com>
2021-01-19 21:18:59 -0500

    stretch partly done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,42 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  let enhancement = item.enhancement
+  if (enhancement < 20) {
+    enhancement += 1
+  }
+  return { ...item, enhancement };
 }
 
 function fail(item) {
-  return { ...item };
+  let enhancement = item.enhancement
+  let durability = item.durability
+  if (enhancement < 15) {
+    durability -= 5
+  } else if (enhancement > 16) {
+    enhancement -= 1
+    durability -= 10
+  } else {
+    durability -= 10
+  }
+
+  return { ...item, enhancement, durability };
 }
 
 function repair(item) {
-  return { ...item };
+  let durability = item.durability
+  durability = 100
+
+  return { ...item, durability };
 }
 
 function get(item) {
-  return { ...item };
+  let enhancement = item.enhancement
+  let name = item.name
+
+  if (enhancement > 0) {
+    name = `[+${enhancement}] ${name}`
+  }
+
+  return { ...item, enhancement, name };
 }
